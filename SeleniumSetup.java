8cfc819f1db48909d1333ed76ae1cc9c344cbbf1
vedha329 <109227338+vedha329@users.noreply.github.com>
2022-07-17 11:26:33 +0530

    Add files via upload

---
+++ SeleniumSetup.java
@@ -0,0 +1,30 @@
+package Week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumSetup {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		WebElement logout = driver.findElement(By.className("decorativeSubmit"));
+		
+		String attribute = logout.getAttribute("value");
+		System.out.println(attribute);
+		if(attribute.equals("logout"))
+				{
+		System.out.println("Successfully logged In");
+
+		}
+	}
+
+	}
